d9595668fd91908449e4c0459412195379f2c649
79817 <mudrik898710@yandex.ru>
2022-12-19 19:09:10 +0300

    ыварвапрва21212рвар12121

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("11221213212122121212122122121212121121!");
+        System.out.println("11221212213212122121212122122121212121121!");
     }
 }
